5c51c0a8111ec64bc9118019e2b5229cb61f70e2
ADRIELE OLIVEIRA DE AQUINO <ADRIELE0471@DIGITALCOLLEGE.LOCAL>
2023-11-11 16:59:05 -0300

    Funcao e Array

---
+++ script.js
@@ -0,0 +1,18 @@
+//Funções:
+//let nome = "adriele";
+//let sobrenome = "Aquino"
+//let iniciais = nome.charAt(0) + sobrenome.charAt(0)
+//alert(iniciais)
+//alert(nome.replace("l","n"))
+//alert(nome.toLowerCase())
+//alert(nome.charAt(0).toUpperCase() + nome.slice(1))
+
+//Array:
+let frutas = ["abacaxi", "banana", "cacau", "damasco"];
+//alert(frutas.length) - mostra a quantidade de produtos da lista(tamanho)
+//alert(frutas.toString()) - mostra o nome dos elementos que tem na lista
+//frutas.push("figo") - adiciona mais elementos na lista
+//Document.write("<h2>Adriele</h2>")
+//frutas.pop()
+
+alert(frutas);
